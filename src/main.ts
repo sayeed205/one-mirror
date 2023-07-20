2d3c2830a27e739534b196d29a1c50b1b6b770fa
Hitarashi <sayeed205@gmail.com>
2023-07-20 21:28:22 +0530

    Update prod to start cmd, use dotenv instead of config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import * as config from 'config';
 import { NestGram } from 'nestgram';
+import { config } from 'dotenv';
+config();
+
 import { AppModule } from './app/app.module';
+import * as process from 'process';
 
 async function bootstrap(): Promise<void> {
-    const bot: NestGram = new NestGram(
-        config.get<string>('BOT_TOKEN'),
-        AppModule,
-    );
+    const bot: NestGram = new NestGram(process.env.BOT_TOKEN, AppModule);
     await bot.start();
 }
 
